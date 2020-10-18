de41a72baf32cc12c7937a16add860561dd2f4bd
Mary <mary@codechrysalis.io>
2020-10-18 23:29:10 +0900

    refactor: Updated clouds with moar clouds

--- src/Components/Clouds.jsx
+++ src/Components/Clouds.jsx
@@ -4,21 +4,22 @@ import Cloud from "./Cloud";
 import { LEFT_LIMIT, RIGHT_LIMIT } from "../constants/constants";
 
 const cloudsArr = [
-  { x: 7, y: 0, z: 0, name: "one" },
-  { x: -5, y: 0, z: 0, name: "two" },
+  { x: 7, y: -0.7, z: 0, name: "one" },
+  { x: 18, y: -1.5, z: 0, name: "two" },
+  { x: 10, y: 1, z: 0, name: "three" },
+  { x: 30, y: 0, z: 0, name: "four" },
 ];
 
 const Clouds = () => {
-  const cloudsRef = useRef([useRef(), useRef()]);
+  const cloudsRef = useRef([useRef(), useRef(), useRef(), useRef()]);
 
   useFrame(() => {
-    // Clouds should drift across the screen
     cloudsRef.current.map((cloud) => {
       if (cloud.current.position.x <= LEFT_LIMIT - 4) {
         // Loop clouds
         return (cloud.current.position.x = RIGHT_LIMIT + 4);
       }
-
+      // Clouds should drift across the screen
       cloud.current.position.x -= 0.1;
     });
   });
