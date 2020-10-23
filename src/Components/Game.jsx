cd899250559134834cfc5a1adac4d6da4f7c71c0
Mary <mary@codechrysalis.io>
2020-10-23 22:08:49 +0900

    refactor: Trying out green color scheme...

--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -29,8 +29,8 @@ const Game = () => {
 
           <directionalLight intensity={0.5} />
           <ambientLight color="#d8d0d1" intensity={0.8} />
-          {/* TODO: Consider if fog is worth using or not, and where to use */}
-          <fog attach="fog" args={["#cc7b32", 1, 20]} />
+          {/* TODO: Decide between the classic halloween colors of green (#85e21f) and orange (#cc7b32) */}
+          <fog attach="fog" args={["#85e21f", 1, 20]} />
 
           <Suspense fallback={<Loading />}>
             <Character />
